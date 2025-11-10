755050d9ae03363c7da91c7fb27b225d7702ef15
Marcos Mesias <marcosmesiaspro@gmail.com>
2025-11-10 18:00:08 -0500

    Se cambio el codigo y quedo el buscador como se esperaba. Se elimino el modudo fuse

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,17 +1,110 @@
-const API = 'https://animedb1.p.rapidapi.com/top/anime?'; //https://animedb1.p.rapidapi.com/top/anime?limit=24 page=1
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'dcff44e6c4mshb0fa2f7d3fb9303p167ac8jsn89b876830305',
-		'x-rapidapi-host': 'animedb1.p.rapidapi.com'
-	}
-};
+const API = 'https://api.jikan.moe/v4/'; // https://api.jikan.moe/v4/anime?
 
 const container = document.querySelector('#container');
 const search = document.querySelector('#input-search');
 const btnSearch = document.querySelector('#btn-search');
 
-async function animeData(apiUrl, optGet) {
+async function dataFetch(apiUrl) {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data;
+}
+
+// Codigo Main pagina principal
+async function animeMain(count) {
+    try {
+        const animeData = await dataFetch(`${API}top/anime?sfw&page=${count}`);
+        //console.log(animeData.data);
+        //console.log(animeData.pagination.last_visible_page);
+        animeData.data.forEach(items => {
+            container.appendChild(divImg(items.title, items.images.jpg.large_image_url));
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function main() {
+    const firstMain = await dataFetch(`${API}top/anime?sfw`); // `${API}top/anime?sfw`
+    console.log(firstMain.data);
+    firstMain.data.forEach(items => {
+        container.appendChild(divImg(items.title, items.images.jpg.large_image_url));
+    });
+
+    for (let i = 2; i <= 4; i++) {
+        setTimeout(() => {
+            animeMain(i);
+        }, i * 600);
+    }
+}
+
+// Codigo del buscador
+
+async function animeSearch(count, title) {
+    try {
+        const query = title;
+        const animeData = await dataFetch(`${API}anime?q=${encodeURIComponent(query)}&sfw&page=${count}`);
+
+        if (animeData.pagination.last_visible_page >= count) {
+            animeData.data.forEach(items => {
+                container.appendChild(divImg(items.title, items.images.jpg.large_image_url));
+            });
+        }
+
+    } catch (error) {
+        console.error(error)
+    }
+    
+}
+
+btnSearch.addEventListener('click', () => {
+    try {
+        document.querySelectorAll('.items').forEach(deleteItems => deleteItems.remove());
+        for (let i = 1; i <= 10; i++) {
+            setTimeout(() => {
+            animeSearch(i, search.value)
+
+            }, i * 1000)
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Codigo del buscador ENTER
+
+search.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        try {
+            document.querySelectorAll('.items').forEach(deleteItems => deleteItems.remove());
+            for (let i = 1; i <= 10; i++) {
+                setTimeout(() => {
+                    animeSearch(i, search.value)
+
+                }, i * 1000)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
+
+// Template HTML
+function divImg(title, img){
+
+    const div = document.createElement('div');
+    div.classList.add('items');
+    div.innerHTML = `<a href="#"><img class="images" src="${img}" alt="Cover del Anime ${title}"></a>
+        <a href="#"><h2>${title}</h2></a>`
+
+    return div;
+}
+
+main();
+
+/* async function animeData(apiUrl, optGet) {
     try {
         const response = await fetch(`${apiUrl}page=1`, optGet);
         const response2 = await fetch(`${apiUrl}page=2`, optGet);
@@ -71,15 +164,6 @@ async function animeData(apiUrl, optGet) {
     }
 }
 
-function divImg(title, img){
-
-    const div = document.createElement('div');
-    div.classList.add('items');
-    div.innerHTML = `<a href="#"><img class="images" src="${img}" alt="Cover del Anime ${title}"></a>
-        <a href="#"><h2>${title}</h2></a>`
-
-    return div;
-}
 
 function notFound() {
     div = document.createElement('div');
@@ -89,4 +173,4 @@ function notFound() {
     return div;
 }
 
-animeData(API, options);
+animeData(API, options); */
